8b1d5f4fc982bc122308ba9227c451f25e055b3c
fatychang <papachang12@gmail.com>
2019-02-18 16:54:47 +0800

    update forced_project_to_2Dimage
    Change function name from findwidthHeight to forced_project_to_2Dimage.
    Add the 2d image_array as one of the returns.

--- myMath.py
+++ myMath.py
@@ -10,31 +10,36 @@ import math
 
 
 '''
-Estimate the width and height from the pointcloud datapoint
+Estimate the width and height of the corresponding 2D image
+from the pointcloud datapoints
+
+The pointcloud_array argument should be a numpy array with size of [num_of_point, 3]
+Return the width(w), height(h), and the image_array itself
 '''
-def findwidthHeight (verts):
+def forced_project_to_2Dimage (pointcloud_array):
     
     # Get the max and min value in the point cloud along x, y, and z axis
-    boundary_max = verts.max(axis=0)
-    boundary_min = verts.min(axis=0)
+    boundary_max = pointcloud_array.max(axis=0) # return the max of [x, y, z] in all datapoints
+    boundary_min = pointcloud_array.min(axis=0) # return the min of [x, y, z] in all datapoints
     
     # Calculate the width and height of the 2D image
-    diff = boundary_max - boundary_min
+    diff = boundary_max - boundary_min # return the legth in all three dimensions [x, y, z]
     
     # Calculate the ratio of x/y (width/height)
     ratio = diff[0]/diff[1]
     
     # find the approximated width of the image (RX^2 = numofpoints)
-    x_square = verts.shape[0]/ratio
+    x_square = pointcloud_array.shape[0]/ratio
     x = math.sqrt(x_square)
     
     # find y via y = ratio * x
     y = ratio * x
     
-    # Round the values and return (width, height)
-    w = round(x, 0)
-    h = round(y, 0)
-    return (w, h)
+    # Round the values to int and return (width, height)
+    w = int(x)
+    h = int(y)
+    image = pointcloud_array.reshape(int(w), int(h), 3)
+    return (w, h, image)
     
 
 
